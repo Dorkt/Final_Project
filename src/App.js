b858a166720bfc76333b19d2bb3ac62eb4cb371a
Dorkt <gabrieldorkt@hotmail.com>
2022-02-10 18:18:28 -0300

    [Main] - Created Header and your style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Api from './api/Api';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './css/Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <div className={styles.header}>Footer</div>
+  )
+}
+
+export default Footer;
